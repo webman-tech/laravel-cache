6590afe750cfac98ac0668af2a0d01ac22b085af
kriss <462679766@qq.com>
2025-07-18 17:09:00 +0800

    feature: normalize commands

--- Command/ClearCommand.php
+++ Command/ClearCommand.php
@@ -2,20 +2,19 @@
 
 namespace WebmanTech\LaravelCache\Command;
 
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\QuestionHelper;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
 use WebmanTech\LaravelCache\Facades\Cache;
 
+#[AsCommand(name: 'cache:clear', description: 'Flush the application cache')]
 class ClearCommand extends Command
 {
-    protected static $defaultName = 'cache:clear';
-    protected static $defaultDescription = 'Flush the application cache';
-
     /**
      * @return void
      */
